ca6b63ece93d057e2a02874615daec430f5b30df
FranckNk <nkeuboufranck@gmail.com>
2022-03-18 01:25:46 -0300

    Configuration et fonctionnement du BMP280 et de l'OLED.

--- src/main.cpp
+++ src/main.cpp
@@ -15,20 +15,20 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 /***************************************************************************
-  This is a library for the BMP280 humidity, temperature & pressure sensor
+	This is a library for the BMP280 humidity, temperature & pressure sensor
 
-  Designed specifically to work with the Adafruit BMP280 Breakout
-  ----> http://www.adafruit.com/products/2651
+	Designed specifically to work with the Adafruit BMP280 Breakout
+	----> http://www.adafruit.com/products/2651
 
-  These sensors use I2C or SPI to communicate, 2 or 4 pins are required
-  to interface.
+	These sensors use I2C or SPI to communicate, 2 or 4 pins are required
+	to interface.
 
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
+	Adafruit invests time and resources providing this open source code,
+	please support Adafruit andopen-source hardware by purchasing products
+	from Adafruit!
 
-  Written by Limor Fried & Kevin Townsend for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
+	Written by Limor Fried & Kevin Townsend for Adafruit Industries.
+	BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
 
@@ -42,22 +42,22 @@ Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println(F("BMP280 Forced Mode Test."));
+	Serial.begin(9600);
+	Serial.println(F("BMP280 Forced Mode Test."));
 
-  //if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
-  if (!bmp.begin(0x76)) {
-    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
-                      "try a different address!"));
-    while (1) delay(10);
-  }
+	//if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+	if (!bmp.begin(0x76)) {
+		Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+											"try a different address!"));
+		while (1) delay(10);
+	}
 
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+	/* Default settings from datasheet. */
+	bmp.setSampling(Adafruit_BMP280::MODE_FORCED,     /* Operating Mode. */
+									Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+									Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+									Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+									Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
 	if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
 		Serial.println(F("SSD1306 allocation failed"));
@@ -77,27 +77,25 @@ void setup() {
 }
 
 void loop() {
-  // must call this to wake sensor up and get new measurement data
-  // it blocks until measurement is complete
-  if (bmp.takeForcedMeasurement()) {
-    // can now print out the new measurements
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
+	// must call this to wake sensor up and get new measurement data
+	// it blocks until measurement is complete
+	if (bmp.takeForcedMeasurement()) {
+		// can now print out the new measurements
+		Serial.print(F("Temperature = "));
+		Serial.print(bmp.readTemperature());
+		Serial.println(" *C");
 
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
+		Serial.print(F("Pressure = "));
+		Serial.print(bmp.readPressure());
+		Serial.println(" Pa");
 
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
-    Serial.println(" m");
+		Serial.print(F("Approx altitude = "));
+		Serial.print(bmp.readAltitude(1013.25)); /* Adjusted to local forecast! */
+		Serial.println(" m");
 
-    Serial.println();
-    delay(2000);
-  } else {
-    Serial.println("Forced measurement failed!");
-  }
-
-
+		Serial.println();
+		delay(2000);
+	} else {
+		Serial.println("Forced measurement failed!");
+	}
 }
