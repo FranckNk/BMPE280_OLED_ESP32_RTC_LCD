a9bfd3cd88b12a52f9fe44b63615a181ccc80786
FranckNk <nkeuboufranck@gmail.com>
2022-03-18 10:23:02 -0300

    Finalisation du projet et fonctionnement de l'horloge, du BMP, et des écrans OLED, et LCD.

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,14 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "DFRobot_RGBLCD1602.h"
 #include <Adafruit_BMP280.h>
+#include "RTClib.h"
+
+RTC_DS3231 rtc;
+
+char Days[7][12] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
@@ -41,6 +48,10 @@ Adafruit_BMP280 bmp; // I2C
 //Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
 //Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
+// Déclaration de la classe LCD pour notre afficheur.
+DFRobot_RGBLCD1602 lcd(/*lcdCols*/16,/*lcdRows*/2);  //16 characters and 2 lines of show 
+
+
 void setup() {
 	Serial.begin(9600);
 	Serial.println(F("BMP280 Forced Mode Test."));
@@ -63,7 +74,41 @@ void setup() {
 		Serial.println(F("SSD1306 allocation failed"));
 		for(;;);
 	}
-	delay(2000);
+
+	// Ajout du code pour initialiser le RTC.
+	#ifndef ESP8266
+		while (!Serial); // wait for serial port to connect. Needed for native USB
+	#endif
+
+	if (! rtc.begin()) {
+		Serial.println("Couldn't find RTC");
+		Serial.flush();
+		while (1) delay(10);
+	}
+	if (rtc.lostPower()) {
+		Serial.println("RTC lost power, let's set the time!");
+		// When time needs to be set on a new device, or after a power loss, the
+		// following line sets the RTC to the date & time this sketch was compiled
+		rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+		// This line sets the RTC with an explicit date & time, for example to set
+		// January 21, 2014 at 3am you would call:
+		// rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  	}
+
+	  /**
+     *  @brief initialize the LCD and master IIC
+     */ 
+    lcd.init();
+    // Print a first message to the LCD.
+		lcd.clear();
+		lcd.setRGB(200, 200, 200);
+		lcd.setCursor(0,0);
+		lcd.print("Getting Date...");
+		lcd.setCursor(0,1);
+		lcd.print("Getting Time...");
+
+	delay(3000);
+	// Initialisation de l'écran OLED.
 	display.clearDisplay();
 
 	display.setTextSize(2);
@@ -79,21 +124,39 @@ void setup() {
 void loop() {
 	// must call this to wake sensor up and get new measurement data
 	// it blocks until measurement is complete
+	DateTime TempsActuel = rtc.now();
 	if (bmp.takeForcedMeasurement()) {
+		lcd.clear();
+		lcd.setRGB(200, 200, 200);
+		lcd.setCursor(0,0);
+		lcd.print(TempsActuel.year(), DEC);
+		lcd.print('/');
+		lcd.print(TempsActuel.month(), DEC);
+		lcd.print('/');
+		lcd.print(TempsActuel.day(), DEC);
+		lcd.setCursor(0,1);
+		lcd.print(" (");
+		lcd.print(Days[TempsActuel.dayOfTheWeek()]);
+		lcd.print(") ");
+		lcd.print(TempsActuel.hour(), DEC);
+		lcd.print(':');
+		lcd.print(TempsActuel.minute(), DEC);
+		lcd.print(':');
+		lcd.print(TempsActuel.second(), DEC);
 		// can now print out the new measurements
 		display.clearDisplay();
 		display.setCursor(0, 0);
-		display.setTextSize(1);
+		display.setTextSize(2);
 		// Display static text
-		display.print("T = ");
+		display.print("T= ");
 		display.print(bmp.readTemperature());
-		display.print(" *C\n");
-		display.print("P = ");
+		display.print(" C\n");
+		display.print("P= ");
 		display.print(bmp.readPressure());
-		display.print(" Pa\n");
-		display.print("Al = ");
+		display.print("Pa\n");
+		display.print("Al= ");
 		display.print(bmp.readAltitude(1013.25));
-		display.print(" m\n");
+		display.print("m\n");
 
 		display.display(); 
 
